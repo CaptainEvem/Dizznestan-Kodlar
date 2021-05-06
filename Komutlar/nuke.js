b8f41f3a77f7e1abc2beecbccd93cb30f1dcd3dc
Dizzness <83604467+Dizzness@users.noreply.github.com>
2021-05-06 11:46:46 +0300

    Update nuke.js

--- Komutlar/nuke.js
+++ Komutlar/nuke.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 exports.run = (client, message, args) => {
-if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Bu Komutu Kullanabilmek İçin **YÖNETİCİ** Yetkisine Sahip Olman Gerek.");
+if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("Bu Komutu Kullanabilmek İçin **YÖNETİCİ** Yetkisine Sahip Olman Gerek. \n https://github.com/Nober-Freelance-Coders/Dizznestan-Kodlar/");
 message.channel.clone().then(knl => {
   let position = message.channel.position;
   knl.setPosition(position);
